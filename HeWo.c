1ef70616c812f499f0883989926de99901dae4a7
Skillicon Technologies <57039021+skillicon@users.noreply.github.com>
2025-03-25 11:55:37 +0530

    Update HeWo.c with addition function
    addition function has been implemented in HeWo.c:
    arugment1: int - the first number to be added
    argument2: int - the second number to be added
    return value: int - sum of the arguments

--- HeWo.c
+++ HeWo.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+int addition(int n1, int n2)
+{
+  return (n1 + n2);
+}
+
 int main()
 {
-  printf("Hello World!\n");
+  int num1, num2, result;
+  printf("Enter 2 numbers: ");
+  scanf("%d%d", &num1, &num2);
+
+  result = addition(num1, num2);
+  printf("Sum of %d and %d is %d", num1, num2, sum);
+  
   return 0;
 }
